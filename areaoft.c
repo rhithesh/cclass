51f9fca9bd0be890f6bda6e068355d0a29db7a5c
rhithesh <rhithesh1947@gmail.com>
2023-06-21 12:36:35 +0530

    triangle area

---
+++ areaoft.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+void main(){
+    int b,h;
+    printf("enter h,b:");
+    scanf("%d %d",&h,&b);
+    float ans= 0.5*b*h;
+    printf("area is %.2f",ans);
+
+}
